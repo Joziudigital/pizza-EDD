504c393c9cf66e7a0149b6a8e565edf4f48b9357
Jorge Zapata <jorge.zapatas@est.iudigital.edu.co>
2026-05-17 10:59:37 -0500

    act

--- pizza.java
+++ pizza.java
@@ -1,3 +1,12 @@
 public class pizza {
-    
+   private String nombre;
+   private String ingredientes;
+
+public pizza(String nombre, String ingredientes){
+    this.nombre=nombre;
+    this.ingredientes=New String[3];
+    for(int=i;i<3;i++){
+        this.ingredientes[i]=ingredientes[i];
+    }
+}
 }
