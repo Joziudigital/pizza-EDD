b7317673dbcdf274849dd5e77d50cf4c05f6000f
Jorge Zapata <jorge.zapatas@est.iudigital.edu.co>
2026-05-17 11:05:19 -0500

    act

--- pizza.java
+++ pizza.java
@@ -1,12 +1,17 @@
 public class pizza {
    private String nombre;
-   private String ingredientes;
+   private String[] ingredientes;
 
-public pizza(String nombre, String ingredientes){
+public pizza(String nombre, String[] ingredientes){
     this.nombre=nombre;
     this.ingredientes=New String[3];
     for(int=i;i<3;i++){
         this.ingredientes[i]=ingredientes[i];
     }
+}
+public String getNombre(){
+    return nombre;
+}
+public String
 }
 }
